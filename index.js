95e6d113c6f37258a22a7cdcd81f17295fdbbbf8
Manishcodelive <manishmaurya13678@gmail.com>
2024-01-03 18:27:08 +0530

    creating a keyboard checker. using tml , css, and javascript.

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let pressedKey;
+
+    document.addEventListener('keyup', function (event) {
+        pressedKey = event.key.toLowerCase(); // Convert to lowercase for case-insensitivity
+        console.log(pressedKey);
+        check();
+    });
+
+    document.addEventListener('keydown', function (event) {
+        pressedKey = event.key.toLowerCase(); // Convert to lowercase for case-insensitivity
+        check();
+    });
+
+    function check() {
+        var keys = document.querySelectorAll('.key');
+
+        keys.forEach(function (key) {
+            const keyId = key.id;
+            
+            if (keyId === pressedKey) {
+                key.classList.add('selected');
+            } else {
+                key.classList.remove('selected');
+            }
+        });
+    }
+});
